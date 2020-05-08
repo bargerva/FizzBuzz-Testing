0abeceb7aa0ab956121b172792412683f2932aaa
Veronica Bargerstock <59888212+bargerva@users.noreply.github.com>
2020-05-08 00:54:45 -0400

    Add files via upload
    finished

---
+++ fizzbuzz_procedural.py
@@ -0,0 +1,11 @@
+# write a loop to iterate through the number 1 to 100
+for x in range (1, 101):
+    if x % 15 == 0:
+        print ('FizzBuzz')
+    elif x % 5 == 0:
+        print ('Buzz')
+    elif x % 3 == 0:
+        print ('Fizz')
+    else:
+        print (x)
+        
